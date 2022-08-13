ce2550b62473422958536cda29b78aa3e5514e73
Seoyoung Yoon <seoyoung7623@naver.com>
2022-08-14 01:40:56 +0900

    2022.08.14

--- 1015.py
+++ 1015.py
@@ -1,1 +1,16 @@
-#1015
+#1015 해결
+num = int(input())
+
+n = list(map(int,input().split()))
+result = []
+for i in range(num):
+    result.append(0)
+
+for i in range(len(n)):
+    result[n.index(min(n))] = i
+    n[n.index(min(n))] = max(n)+1
+#min의 인덱스는 가장 왼쪽에 있는 최소 인덱스로 반환한다.
+
+for i in result:
+    print(i,end=' ')
+
---
+++ 문법,기초/05 min max연습.py
@@ -0,0 +1,3 @@
+list = [4,6,3,7,2,9]
+
+print(list.index(min(list)))
