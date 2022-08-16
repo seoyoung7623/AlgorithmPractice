2229f637b603d8aa345f1b08a14eafbf24d5b72e
Seoyoung Yoon <seoyoung7623@naver.com>
2022-08-16 23:16:32 +0900

    2022.08.16

--- 1463.py
+++ 1463.py
@@ -11,3 +11,13 @@ for i in range(4,n+1):
     if i % 3 == 0:
         dp[i] = min(dp[i//3]+1,dp[i])
 print(dp[n])
+
+# n = int(input())
+# arr = [0]*(n+1)
+# for i in range(2,n+1):
+#     arr[i] += arr[i-1] + 1 
+#     if i%3 == 0 and arr[i//3]+1 < arr[i]:
+#         arr[i] = arr[i//3] + 1
+#     if i%2 == 0 and arr[i//2] +1 < arr[i]:
+#         arr[i] = arr[i//2] + 1 
+# print(arr[n])
---
+++ 2839.py
@@ -0,0 +1,51 @@
+n = int(input())
+
+# dp = [-1,-1,-1,1,-1,1]
+
+# for i in range(6,n+1):
+#     if dp[i-3] 
+#     dp[i] = min(dp[i-3]+1,dp[i-5]+1)
+minlist = []
+cnt = 0
+# while n>0:
+#     if n % 3 == 0:
+#         cnt+=(n//3)
+#         minlist.append(cnt)
+#         cnt = 0
+#     n = n-5
+#     if n<0:
+#         break
+#     elif n == 0:
+#         cnt +=1
+#         minlist.append(cnt)
+#     else:
+#         cnt+=1
+
+# if min(minlist) == 0:
+#     print(-1)
+# else:
+#     print(min(minlist))
+
+while n >= 0:
+    if n % 5 == 0:
+        cnt += (n//5)
+        print(cnt)
+        break
+    n -= 3
+    cnt+=1
+else:
+    print(-1)
+
+# cnt = 0
+# if (n%5)%3 == 0:
+#     cnt += n//5
+#     cnt += (n%5)//3
+#     print(cnt)
+#     minList.append(cnt)
+#     cnt = 0
+# elif n%3 == 0:
+#     cnt += n//3
+#     print(cnt)
+# else:
+#     print(-1)
+ 
