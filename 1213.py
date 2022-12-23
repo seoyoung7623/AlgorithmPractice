4f8ac90235807d70d794ab7cc62cdaf038e78ee8
Seoyoung Yoon <seoyoung7623@naver.com>
2022-12-24 01:18:17 +0900

    20221224

--- 1213.py
+++ 1213.py
@@ -1,2 +1,29 @@
 #1213 팰린드롬 만들기
 # 그리디 알고리즘 구현 문자열
+
+str = list(input())
+strList = []
+oddcnt = 0
+strSet = list(set(str)) #집합으로 바꾸어 각 문자 집합 확인
+center = ""
+
+for i in strSet:
+    if str.count(i)%2 !=0:
+        center = i * str.count(i)
+        oddcnt +=1
+        str.remove(i)
+    if oddcnt>=2:
+        print("I'm Sorry Hansoo")
+        break
+print(str)
+        
+
+for i in str:
+    print(str.count(i))
+    if str.count(i)%2 == 0:
+        strList.append(i)
+    else:
+        oddcnt +=1
+    if oddcnt >= 2:
+        print("I'm Sorry Hansoo")
+        break
