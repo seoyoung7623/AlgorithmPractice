ce5a056ca0e847dffd56ffc72841a1f287d5937b
Seoyoung Yoon <seoyoung7623@naver.com>
2022-12-26 14:20:55 +0900

    20221226

--- 1213.py
+++ 1213.py
@@ -8,22 +8,33 @@ strSet = list(set(str)) #집합으로 바꾸어 각 문자 집합 확인
 center = ""
 
 for i in strSet:
-    if str.count(i)%2 !=0:
-        center = i * str.count(i)
+    if str.count(i)%2 !=0: #홀수발견
+        center += i
         oddcnt +=1
-        str.remove(i)
-    if oddcnt>=2:
-        print("I'm Sorry Hansoo")
-        break
-print(str)
+        str.remove(i) #remove는 가장 먼저 발견한 것을 삭제한다.
+        if oddcnt>=2:
+            break
+
+if oddcnt>=2:
+    print("I'm Sorry Hansoo")
+else:
+    str.sort() 
+    res =""
+    for i in range(0,len(str),2): #제일 중요(짝수개 이므로)
+        res += str[i]
+    print(res + center + res[::-1])
+    
+
+# print(str)
+# print(center)
         
 
-for i in str:
-    print(str.count(i))
-    if str.count(i)%2 == 0:
-        strList.append(i)
-    else:
-        oddcnt +=1
-    if oddcnt >= 2:
-        print("I'm Sorry Hansoo")
-        break
+# for i in str:
+#     print(str.count(i))
+#     if str.count(i)%2 == 0:
+#         strList.append(i)
+#     else:
+#         oddcnt +=1
+#     if oddcnt >= 2:
+#         print("I'm Sorry Hansoo")
+#         break
