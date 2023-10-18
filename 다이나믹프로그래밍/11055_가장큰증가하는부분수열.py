4a7365b31329862c403bd2ef1cea843a215e352d
SeoYoung <seoyoung7623@naver.com>
2023-10-18 13:36:27 +0900

    2023.10.18 주석수정

--- 다이나믹프로그래밍/11055_가장큰증가하는부분수열.py
+++ 다이나믹프로그래밍/11055_가장큰증가하는부분수열.py
@@ -9,6 +9,6 @@ for i in range(1,n):
     for j in range(i):
         if arr[i]>arr[j]:
             dp[i] = max(dp[i],dp[j]+arr[i]) #증가하는 값이여야한다.
-        # else: # 이 부분이 없으면 틀리는 이유? i와 j가 같거나 j가 작은경우 갱신 
-        #     dp[i] = max(dp[i],arr[i]) 
+        else: # 이 부분이 없으면 틀리는 이유? i와 j가 같거나 j가 작은경우 갱신 
+            dp[i] = max(dp[i],arr[i]) 
 print(max(dp))
