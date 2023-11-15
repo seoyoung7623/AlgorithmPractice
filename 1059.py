8674f96640e6c4e93f4ce77204e3951fdc2ff80d
SeoYoung <seoyoung7623@naver.com>
2023-11-15 22:38:33 +0900

    2023.11.15 미해결문제 해결

--- 1059.py
+++ 1059.py
@@ -1,8 +1,8 @@
-# 좋은 구간 미해결
-
+# 좋은 구간
 cnt = int(input())
 list = list(map(int, input().split()))
 num = int(input())
+list.append(0) # 리스트의 값들보다 작은 num 이 있을수 있기때문에 추가
 list.sort()
 
 last = int(min(list))
@@ -12,6 +12,7 @@ else:
     for i in list:
         if last <num < i:
             print((num-last-1)*(i-num)+(i-num-1))
+            break
         last = int(i) 
 
 
