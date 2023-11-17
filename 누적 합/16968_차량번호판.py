5d0ec1010f1d67a67dd0e4e7c79f62cc1a9b0b02
SeoYoung <seoyoung7623@naver.com>
2023-11-17 23:58:17 +0900

    2023.11.17

---
+++ 누적 합/16968_차량번호판.py
@@ -0,0 +1,23 @@
+# 16968 차량번호판 B1
+'''
+중복이 3개 이상인 경우 10, 9, 8 이 아니라 10,9,9!
+'''
+ans = input()
+result = 0
+if ans[0] == 'c':
+    result = 26
+else:
+    result = 10
+for i in range(1,len(ans)):
+    if ans[i] == 'c':
+        if ans[i-1] == 'c':
+            result *= 25
+        else:
+            result * 26
+    else:
+        if ans[i-1] == 'd':
+            result *= 9
+        else:
+            result *= 10
+
+print(result)
