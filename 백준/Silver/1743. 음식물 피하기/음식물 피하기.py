a2a1c0ae8ac65198218bf56c621e32868ee6762c
SeoYoung <seoyoung7623@naver.com>
2025-04-23 10:46:48 +0900

    [Silver I] Title: 음식물 피하기, Time: 64 ms, Memory: 34976 KB -BaekjoonHub

---
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -0,0 +1,34 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+N,M,K = map(int,input().split()) #세로,가로,개수
+grid = [[0]*M for _ in range(N)]
+visited = [[0]*M for _ in range(N)]
+answer = 0
+for _ in range(K):
+    x,y = map(int,input().split())
+    grid[x-1][y-1] = 1
+
+diractions = [(1,0),(0,1),(-1,0),(0,-1)]
+def BFS(x,y):
+    q = deque([(x,y)])
+    visited[x][y] = 1
+    cnt = 1
+    while q:
+        x,y = q.popleft()
+        for dx,dy in diractions:
+            nx = x + dx
+            ny = y + dy
+            if 0<=nx<N and 0<=ny<M:
+                if grid[nx][ny] == 1 and visited[nx][ny] == 0:
+                    cnt += 1
+                    visited[nx][ny] = 1
+                    q.append((nx,ny))
+    return cnt
+
+for i in range(N):
+    for j in range(M):
+        if grid[i][j] == 1:
+            answer = max(answer,BFS(i,j))
+            
+print(answer)
