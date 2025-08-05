fc1653cb52571bb20feeec2a4edc3e34cb26755c
SeoYoung <seoyoung7623@naver.com>
2025-08-05 15:26:16 +0900

    [Bronze III] Title: 별 찍기 - 15, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+if N == 1:
+    print('*')
+else:
+    for i in range(N):
+        if i == 0:
+            print(' '*(N-1)+'*')
+        elif i == N-1:
+            print('*'+' '*(2*i-1)+'*')
+        else:
+            print(' '*(N-i-1)+'*'+' '*(2*i-1)+'*')
