baee06a7fe5888bda1770c5ecd89e9d519ca57ac
SeoYoung <seoyoung7623@naver.com>
2025-09-03 14:52:31 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -3,10 +3,12 @@
 최대 값만 출력하는 방법..?
 1등 점수가 한명이 아닐 수 있잖아
 */
-select sum(gr.score) as SCORE, em.emp_no, em.emp_name, em.position, em.email
+select x.score, x.emp_no, x.emp_name, x.position, x.email from (
+select sum(gr.score) as SCORE, em.emp_no, em.emp_name, em.position, em.email,
+    rank() over (order by sum(gr.score) desc) as rnk
 from hr_department de
 join hr_employees em on de.dept_id = em.dept_id
 join hr_grade gr on gr.emp_no = em.emp_no
 group by em.emp_no
-order by SCORE DESC
-limit 1;
+) x where x.rnk = 1;
+# limit 1;
