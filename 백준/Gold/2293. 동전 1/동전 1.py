a360cd7f056d1e1289671b50a8ea59103ccd53f1
SeoYoung <seoyoung7623@naver.com>
2025-11-15 22:11:53 +0900

    [Gold IV] Title: 동전 1, Time: 188 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,10 +1,9 @@
-import sys
-input = sys.stdin.readline
-
 n,k = map(int,input().split())
-money = list(int(input()) for _ in range(n))
-dp = [1]+[0]*k
-for coin in money:
-    for c in range(coin,k+1):
-        dp[c] += dp[c-coin]
+
+coins = [int(input()) for _ in range(n)]
+dp = [1] + ([0] * k)
+
+for c in coins:
+    for i in range(c,k+1):
+        dp[i] += dp[i-c]
 print(dp[k])
